08f422d7c25b60e254812f6ebf5a6a912f73ff07
Rémy Beumier <beumieremy@gmail.com>
2021-11-12 12:16:02 +0100

    theme test 2

--- components/layout.js
+++ components/layout.js
@@ -6,37 +6,22 @@ import { useEffect, useState } from 'react'
 import useLocalStorage from 'use-local-storage';
 
 export default function Layout(props) {
-  // let [theme, setTheme] = useState(() => {
-  //   if (typeof window !== 'undefined') {
-  //     return localStorage.getItem('theme') || false;
-  //   }
-  // });
-  // const test = () => {
-  //   setTheme(!theme);
-  //   localStorage.setItem('theme', !theme);
-  //   console.log(localStorage.getItem('theme'), !theme);
-  // }
-  // let defaultDark;
-  // if (typeof window !== 'undefined') {
-  // defaultDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
-  // }
-  let localTheme;
-  const [theme, setTheme] = useLocalStorage('theme', localTheme || 'light');
+  // fix local storage to be listened on page refresh
+  // https://css-tricks.com/easy-dark-mode-and-multiple-color-themes-in-react/
+  const [theme, setTheme] = useLocalStorage('theme');
 
   const switchTheme = () => {
     const newTheme = theme === 'light' ? 'dark' : 'light';
     setTheme(newTheme);
   }
 
-
   return (
-    <main className={theme === 'dark' ? 'dark' : 'light'}>
+    <main className={theme}>
       <Head>
         <title>{props.title}</title>
         <meta name='description' content={props.description} />
       </Head>
       <Header onClick={switchTheme} theme={theme} />
-      {/* <Header onClick={() => test()} theme={theme} /> */}
       {props.children}
       <Footer />
       <MobileNav />
