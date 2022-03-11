f8fc4e9ff08beda2d9869b502c0239a5c76ad6a6
Rémy Beumier <beumieremy@gmail.com>
2022-03-11 17:27:21 +0100

    github action fix

--- pages/_error.js
+++ pages/_error.js
@@ -17,10 +17,9 @@ function Error({ statusCode }) {
   useEffect(() => {
     // reload page to bypass safari error of window undefined
     router.reload();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
-  // avoid infinite reload, is removing router from dependency array enough?
-  // }, [router]);
+    // }, []);
+    // avoid infinite reload, is removing router from dependency array enough?
+  }, [router]);
 
   return (
     <div style={styleDiv}>
