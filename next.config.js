f94278ba0c5fa436899af805a38ab6120d5e2683
Rémy Beumier <beumieremy@gmail.com>
2022-03-22 11:04:39 +0100

    optimised images + dark code highlighter

--- next.config.js
+++ next.config.js
@@ -1,12 +1,17 @@
-module.exports = {
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+});
+
+module.exports = withBundleAnalyzer({
+  reactStrictMode: true,
   webpack5: false,
   target: 'serverless',
   eslint: {
     ignoreDuringBuilds: true,
   },
   webpack: function (config) {
-      config.module.rules.push({test:  /\.md$/, use: 'raw-loader'})
-      config.module.rules.push({test: /\.yml$/, use: 'raw-loader'})
-      return config
+    config.module.rules.push({ test: /\.md$/, use: 'raw-loader' });
+    config.module.rules.push({ test: /\.yml$/, use: 'raw-loader' });
+    return config;
   },
-}
+});
