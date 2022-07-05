22c07474746e4b733fb673cd50b11689faead90a
Rémy Beumier <beumieremy@gmail.com>
2022-07-05 13:40:49 +0200

    nextjs v12 upgrade fix2

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 
 module.exports = withBundleAnalyzer({
   reactStrictMode: true,
-  // target: 'serverless',
+  target: 'serverless',
   eslint: {
     ignoreDuringBuilds: true,
   },
