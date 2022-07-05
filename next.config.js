abc609ec949693be479aa32e09098ce1ab54dd3e
Rémy Beumier <beumieremy@gmail.com>
2022-07-05 22:43:02 +0200

    nextjs v12 upgrade fix5

--- next.config.js
+++ next.config.js
@@ -1,10 +1,9 @@
-const withBundleAnalyzer = require('@next/bundle-analyzer')({
-  enabled: process.env.ANALYZE === 'true',
-});
-
-module.exports = withBundleAnalyzer({
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   reactStrictMode: true,
   eslint: {
     ignoreDuringBuilds: true,
   },
-});
+};
+
+module.exports = nextConfig;
