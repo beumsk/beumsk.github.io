d4ce771c24457bda8af209c8dad1cb0d48dd3ee0
beumsk <beumieremy@gmail.com>
2024-03-26 17:27:43 +0000

    Deploying to gh-pages from @ beumsk/beumsk.github.io@80c531d033f2ff7922f2f7bd8c5e9fc921ae7246 🚀

---
+++ _next/static/O6RrYZsJh05LwAbwbLwK6/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002F","\u002F404","\u002Fblog","\u002Fprojects","\u002Fprojects\u002F[slug]","\u002Fresume-remy-beumier"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
